7fc1394fdd357670734b81b768ab2c52063a7fbc
SantinoGiordano <santinogiordano13@gmail.com>
2025-07-01 13:21:04 -0400

    created a data feild for buttons

---
+++ Data/buttonsHomePage.tsx
@@ -0,0 +1,13 @@
+
+const buttonsHomePage = () => {
+  const data = [
+    { id: 1, name: "Thanksgiving", address: "/thanksgiving" },
+    { id: 2, name: "Our Menu", address: "/menu" },
+    { id: 3, name: "Catering Menu", address: "/cateringMenu" },
+    { id: 4, name: "Upcoming Events", address: "/events" },
+  ];
+
+  return data;
+};
+
+export default buttonsHomePage;
