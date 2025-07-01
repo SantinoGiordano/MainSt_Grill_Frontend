e2352d061810ddb394f1d5a76cca3e2e9b7138b9
SantinoGiordano <santinogiordano13@gmail.com>
2025-07-01 10:38:56 -0400

    removed

--- app/page.tsx
+++ app/page.tsx
@@ -9,13 +9,7 @@ export default function Home() {
         <Nav/>
       </div>
       <div>
-        <Link
-          href="https://order.online/store/main-street-deli-restaurant-&-catering-agawam-25125723/?hideModal=true&pickup=true"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Order Online 
-        </Link>
+       
       </div>
     </>
   );
