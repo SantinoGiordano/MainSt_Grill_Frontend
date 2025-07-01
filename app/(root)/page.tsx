4f02b561d166ef102c8a420d5d7e31373038f552
SantinoGiordano <santinogiordano13@gmail.com>
2025-07-01 19:52:19 -0400

    added google fonts, removed footer / nav

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,20 +1,32 @@
 "use client";
 
-import Nav from "./components/Nav";
 import Link from "next/link";
 import buttonsHomePage from "@/Data/buttonsHomePage";
-import Footer from "./components/footer";
+import Head from "next/head";
 export default function Home() {
-  // const [items, setItems] = useState([""])
-
   return (
     <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <div
         className="relative h-[75vh] bg-fixed bg-center bg-cover"
         style={{ backgroundImage: 'url("/sandwitch.jpg")' }}
       >
-        <Nav />
-        <div className="absolute inset-0 flex items-center justify-center">
+
+        <div
+          style={{ fontFamily: "Playfair Display, serif" }}
+          className="absolute inset-0 flex items-center justify-center fontFamily-playfair"
+        >
           <div className="flex flex-col items-center justify-center bg-red-950 bg-opacity-50 p-6 rounded-md shadow-lg">
             <p className="text-2xl md:text-3xl font-bold text-black bg-white px-6 py-4 rounded tracking-wide leading-relaxed">
               Open Monday - Saturday
@@ -29,7 +41,8 @@ export default function Home() {
         </div>
       </div>
       <hr className="border-t-2 border-red-950 my-8 ml-10 mr-10" />
-      <div className="flex flex-wrap justify-center gap-4 mt-8 px-4 pt-10">
+      <div 
+      className="flex flex-wrap justify-center gap-4 mt-8 px-4 pt-10">
         {buttonsHomePage.length > 0 ? (
           buttonsHomePage.map((item, index) => (
             <Link
@@ -44,7 +57,10 @@ export default function Home() {
           <p className="text-center text-gray-500">No buttons available.</p>
         )}
       </div>
-      <Footer/>
+      <div
+      className="p-10">
+
+      </div>
     </>
   );
 }
