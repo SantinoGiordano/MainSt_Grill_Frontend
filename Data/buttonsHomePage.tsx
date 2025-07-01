646321fa7433785256981a0a1fd34160dd787bcf
SantinoGiordano <santinogiordano13@gmail.com>
2025-07-01 13:34:46 -0400

    exported data as an array not a function

--- Data/buttonsHomePage.tsx
+++ Data/buttonsHomePage.tsx
@@ -1,13 +1,8 @@
-
-const buttonsHomePage = () => {
-  const data = [
-    { id: 1, name: "Thanksgiving", address: "/thanksgiving" },
-    { id: 2, name: "Our Menu", address: "/menu" },
-    { id: 3, name: "Catering Menu", address: "/cateringMenu" },
-    { id: 4, name: "Upcoming Events", address: "/events" },
-  ];
-
-  return data;
-};
+const buttonsHomePage = [
+  { id: 1, name: "Thanksgiving", address: "/thanksgiving" },
+  { id: 2, name: "Our Menu", address: "/menu" },
+  { id: 3, name: "Catering Menu", address: "/cateringMenu" },
+  { id: 4, name: "Upcoming Events", address: "/events" },
+];
 
 export default buttonsHomePage;
