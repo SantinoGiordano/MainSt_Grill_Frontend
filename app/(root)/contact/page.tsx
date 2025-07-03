3b7b2846e055f754234247b5766cda65aedfcb91
SantinoGiordano <santinogiordano13@gmail.com>
2025-07-03 11:51:23 -0400

    start to a contact page

---
+++ app/(root)/contact/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function contact() {
+  return (
+    <div>
+      <div
+        className="relative h-[75vh] bg-fixed bg-center bg-cover"
+        style={{ backgroundImage: 'url("/dinningroom.avif")' }}
+      >
+
+        hello
+      </div>
+    </div>
+  );
+}
+
