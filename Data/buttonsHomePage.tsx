fb98565e8eb497985a5e5ca9fa04774d982ee4b0
SantinoGiordano <santinogiordano13@gmail.com>
2025-07-02 22:05:41 -0400

    cahnged data format

--- Data/buttonsHomePage.tsx
+++ Data/buttonsHomePage.tsx
@@ -1,7 +1,7 @@
 const buttonsHomePage = [
   { id: 1, name: "Thanksgiving", address: "/thanksgiving" },
   { id: 2, name: "Our Menu", address: "/menu" },
-  { id: 3, name: "Catering Menu", address: "/cateringMenu" },
+  { id: 3, name: "Catering Menu", address: "/CateringMenu.pdf" },
   { id: 4, name: "Upcoming Events", address: "/events" },
 ];
 
