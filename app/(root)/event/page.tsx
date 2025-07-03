3b2e0d1985f786ea9b0539c46b07a1f5a66aa940
SantinoGiordano <santinogiordano13@gmail.com>
2025-07-03 08:56:09 -0400

    created a page for background img on event

---
+++ app/(root)/event/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Events = () => {
+  return(
+    <>
+      <div
+        className="relative h-[75vh] bg-fixed bg-center bg-cover"
+        style={{ backgroundImage: 'url("/eventBanners.jpg")' }}
+      >
+
+      </div>
+    </>
+  )
+};
+
+export default Events;
