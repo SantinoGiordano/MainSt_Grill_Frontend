cd29bd1957511b81d8e5bba3eeeabfa20c971a16
SantinoGiordano <santinogiordano13@gmail.com>
2025-07-07 12:31:12 -0400

    removed all link tags for fonts becasue next already handles fonts

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -18,10 +18,6 @@ export default function Home() {
           href="https://fonts.gstatic.com"
           crossOrigin=""
         />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
-          rel="stylesheet"
-        />
       </Head> 
       <div
         className="relative h-[75vh] bg-fixed bg-center bg-cover"
--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -18,10 +18,6 @@ const Nav = () => {
           href="https://fonts.gstatic.com"
           crossOrigin=""
         />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
-          rel="stylesheet"
-        />
       </Head>
 
       <nav
