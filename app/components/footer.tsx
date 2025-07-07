b94a096ccb890ad08c118a541e23bf9803076980
SantinoGiordano <santinogiordano13@gmail.com>
2025-07-07 11:30:47 -0400

    removed multiple font loads

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -13,10 +13,6 @@ const Footer = () => {
           href="https://fonts.gstatic.com"
           crossOrigin=""
         />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
-          rel="stylesheet"
-        />
       </Head>
       <div
         style={{ fontFamily: "Playfair Display, serif" }}
