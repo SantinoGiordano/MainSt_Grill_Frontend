61df4356d921e8a391364057654e9ef9d57161c1
SantinoGiordano <santinogiordano13@gmail.com>
2025-07-09 10:09:12 -0400

    altered the spacing and design of mobile applications

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -18,14 +18,13 @@ export default function Home() {
           href="https://fonts.gstatic.com"
           crossOrigin=""
         />
-      </Head> 
+      </Head>
+      {/* This only shows on md+ with background */}
       <div
-        className="relative h-[75vh] bg-fixed bg-center bg-cover"
+        className="hidden md:flex relative h-[75vh] bg-fixed bg-center bg-cover"
         style={{ backgroundImage: 'url("/centerStringSandwitch.jpg")' }}
       >
-        <div
-          className="absolute inset-0 flex items-center justify-center fontFamily-playfair"
-        >
+        <div className="absolute inset-0 flex items-center justify-center fontFamily-playfair">
           <div className="flex flex-col items-center justify-center bg-red-950 bg-opacity-50 p-6 rounded-md shadow-lg">
             <p className="text-2xl md:text-3xl font-bold text-black bg-white px-6 py-4 rounded tracking-wide leading-relaxed">
               Open Monday - Saturday
@@ -39,6 +38,24 @@ export default function Home() {
           </div>
         </div>
       </div>
+
+      {/* This shows on small screens */}
+      <div className="pt-30">
+      <div className=" items-center justify-center fontFamily-playfair">
+          <div className="flex flex-col items-center justify-center bg-red-950 bg-opacity-50 p-6 rounded-md shadow-lg">
+            <p className="text-2xl md:text-3xl font-bold text-black bg-white px-6 py-4 rounded tracking-wide leading-relaxed">
+              Open Monday - Saturday
+            </p>
+            <p className="text-white text-xl md:text-2xl mt-2 tracking-wide leading-relaxed">
+              10AM - 6PM
+            </p>
+            <p className="text-lg text-white md:text-xl mt-1 tracking-wide leading-relaxed">
+              Temporarily Closed Sundays
+            </p>
+          </div>
+        </div>
+      </div>
+
       <hr className="border-t-2 border-red-950 my-8 ml-10 mr-10" />
       <div className="flex flex-wrap justify-center gap-4 mt-8 px-4 pt-10">
         {buttonsHomePage.length > 0 ? (
@@ -46,7 +63,7 @@ export default function Home() {
             <Link
               key={index}
               href={item.address}
-               aria-label={`Navigate to ${item.name}`}
+              aria-label={`Navigate to ${item.name}`}
               className="shadow-2xl w-48 h-20 flex items-center justify-center bg-red-950 text-white text-xl px-6 py-3 rounded  hover:bg-red-800 transition duration-300 text-center"
             >
               {item.name}
