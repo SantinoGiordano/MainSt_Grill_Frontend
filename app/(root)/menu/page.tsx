ab4ceb4134153dc19bd85fe9134001058b3716c6
SantinoGiordano <santinogiordano13@gmail.com>
2025-07-09 10:46:42 -0400

    created a mobile version of menu

--- app/(root)/menu/page.tsx
+++ app/(root)/menu/page.tsx
@@ -3,22 +3,24 @@ import Image from "next/image";
 export default function Footer() {
   return (
     <>
-      <div className=" text-center h-[100vh] flex items-center justify-center">
-        <div className="bg-red-950 flex flex-col items-center justify-center p-10 rounded-lg shadow-lg hover:ease-in-out hover:scale-105 transition-transform">
-          <Link
-            className="text-2xl font-bold text-white hover:text-pink-200 transition-colors"
-            href={"/141MainStreetMenu.pdf"}
-          >
-            Click Here to Check Out Our Menu
-            <Image
-              loading="lazy"
-              alt="Siverware"
-              className="mx-auto my-4"
-              src="/siverware.jpg"
-              height={250}
-              width={250}
-            />
-          </Link>
+      <div className=" text-center h-[100vh] flex items-center justify-center m-5 p-10 md:m-0 md:p-0">
+        <div className=" text-center h-[100vh] flex items-center justify-center">
+          <div className="bg-red-950 flex flex-col items-center justify-center p-10 rounded-lg shadow-lg hover:ease-in-out hover:scale-105 transition-transform">
+            <Link
+              className="text-2xl font-bold text-white hover:text-pink-200 transition-colors"
+              href={"/141MainStreetMenu.pdf"}
+            >
+              Click Here to Check Out Our Menu
+              <Image
+                loading="lazy"
+                alt="Siverware"
+                className="mx-auto my-4"
+                src="/siverware.jpg"
+                height={250}
+                width={250}
+              />
+            </Link>
+          </div>
         </div>
       </div>
     </>
