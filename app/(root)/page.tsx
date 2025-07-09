0be59fac6c84d48107468425bbc8621934757d23
SantinoGiordano <santinogiordano13@gmail.com>
2025-07-09 10:14:37 -0400

    altered coloed for mobile

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -40,9 +40,9 @@ export default function Home() {
       </div>
 
       {/* This shows on small screens */}
-      <div className="pt-30">
+      <div className="pt-40 m-8">
       <div className=" items-center justify-center fontFamily-playfair">
-          <div className="flex flex-col items-center justify-center bg-red-950 bg-opacity-50 p-6 rounded-md shadow-lg">
+          <div className="flex flex-col items-center justify-center bg-black bg-opacity-50 p-6 rounded-md shadow-lg">
             <p className="text-2xl md:text-3xl font-bold text-black bg-white px-6 py-4 rounded tracking-wide leading-relaxed">
               Open Monday - Saturday
             </p>
@@ -64,7 +64,7 @@ export default function Home() {
               key={index}
               href={item.address}
               aria-label={`Navigate to ${item.name}`}
-              className="shadow-2xl w-48 h-20 flex items-center justify-center bg-red-950 text-white text-xl px-6 py-3 rounded  hover:bg-red-800 transition duration-300 text-center"
+              className="shadow-2xl w-48 h-20 flex items-center justify-center bg-black text-white text-xl px-6 py-3 rounded  hover:bg-red-800 transition duration-300 text-center"
             >
               {item.name}
             </Link>
