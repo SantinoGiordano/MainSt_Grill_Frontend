c187fc21e411e261a424a4f279e63a79b1290f31
SantinoGiordano <santinogiordano13@gmail.com>
2025-07-09 10:45:39 -0400

    created a secondary layout for main app, fixed hydration error

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,22 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Main Street Grill",
+  description:
+    "Main Street Grill in Agawam, MA. Open Monday-Saturday. Order online, view weekly specials, catering, and more!",
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -5,26 +24,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-
-        <head>
-            <meta
-            name="description"
-            content="Main Street Grill in Agawam, MA. Open Monday-Saturday. Order online, view weekly specials, catering, and more!"
-            />
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin=""
-            />
-        </head>
-        <body className="antialiased">
-            <div className="bg-gray-100">
-
-            {children}
-            </div>
-
-        </body>
+      <head>
+        <meta name="description" content={metadata.description} />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <div className="bg-gray-100">
+          <main className="flex-grow">{children}</main>
+        </div>
+      </body>
     </html>
   );
 }
