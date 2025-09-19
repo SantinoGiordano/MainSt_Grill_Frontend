b80d5cb66fe504e66cfdb842ce06aa23dd341a61
SantinoGiordano <santinogiordano13@gmail.com>
2025-09-19 10:27:36 -0400

    throw new error for temp on weekly specails

--- app/(root)/weeklySpecial/page.tsx
+++ app/(root)/weeklySpecial/page.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 
 const page = () => {
-  return <div className="p-40">page</div>;
+  throw new Error("Test Error Page");
+  return <div className="p-40">
+    page
+  </div>;
 };
 
 export default page;
